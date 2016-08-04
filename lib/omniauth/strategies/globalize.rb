a2bef12dc231db2dbba3e07a705b91c1cf561c69
Michael Berkovich <michael@translationexchange.com>
2016-08-04 16:22:27 -0700

    added support for dev env

--- lib/omniauth/strategies/globalize.rb
+++ lib/omniauth/strategies/globalize.rb
@@ -4,19 +4,27 @@ module OmniAuth
   module Strategies
     class Globalize < OmniAuth::Strategies::OAuth2
 
-      option :client_options, {
-        :site           => 'https://api.globalize.com/v2',
-        :authorize_url  => 'https://globalize.com/oauth/authorize',
-        :token_url      => 'https://api.globalize.com/v2/oauth/token'
-      }
+      if defined?(Rails) and %w(test development dev).include?(Rails.env.to_s)
+        option :client_options, {
+            :site => 'http://globalize.lvh.me:3030/api/v1',
+            :authorize_url => 'http://globalize.lvh.me:3030/oauth/authorize',
+            :token_url => 'http://globalize.lvh.me:3030/oauth/token'
+        }
+      else
+        option :client_options, {
+            :site => 'https://globalize.io/api/v1',
+            :authorize_url => 'https://globalize.io/oauth/authorize',
+            :token_url => 'https://globalize.io/oauth/token'
+        }
+      end
 
       option :name, 'globalize'
 
       option :access_token_options, {
-        :header_format => 'OAuth %s',
-        :param_name => 'access_token'
+          :header_format => 'OAuth %s',
+          :param_name => 'access_token'
       }
-      
+
       option :authorize_options, [:scope, :display]
 
       def request_phase
@@ -24,25 +32,21 @@ module OmniAuth
       end
 
       uid { raw_info['id'] }
-      
+
       info do
         prune!({
-          'id'             => raw_info['id'],
-          'name'           => raw_info['name'],
-          'first_name'     => raw_info['first_name'],
-          'last_name'      => raw_info['last_name'],
-          'email'          => raw_info['email'],
-          'gender'         => raw_info['gender'],
-          'mugshot'        => raw_info['mugshot']
-        })
+                   'id' => raw_info['id'],
+                   'name' => raw_info['name'],
+                   'email' => raw_info['email']
+               })
       end
-      
-      extra do 
-        { 'user' =>  prune!(raw_info) }
+
+      extra do
+        {'user' => prune!(raw_info)}
       end
-      
+
       def raw_info
-        @raw_info ||= access_token.get('/account').parsed
+        @raw_info ||= access_token.get('users/me').parsed
       end
 
       def authorize_params
